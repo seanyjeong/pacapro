2f6f4a8111ca88181aceecad5076d516741c5596
seanyjeong <seanyjeong@gmail.com>
2025-12-29 20:20:09 +0900

    chore: v3.0.15 버전 업데이트

--- src/components/version-checker.tsx
+++ src/components/version-checker.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from 'react';
 
 // package.json의 버전과 동일하게 유지
-const APP_VERSION = '3.0.14';
+const APP_VERSION = '3.0.15';
 
 export function VersionChecker() {
     useEffect(() => {
